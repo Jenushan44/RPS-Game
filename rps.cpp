0fec7d5055860e04682020fd0bb0f72e62ea0065
Jenushan Premkumar <jenushan447@gmail.com>
2024-07-27 10:41:09 -0400

    Computer Choice function added

--- rps.cpp
+++ rps.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+void computerChoice(char computer);
 
 int main() {
-    
+
+    char computer;
+
+    std::cout<<"Welcome to the Rock Paper Scissors Game";
+    computerChoice(computer);
+
+}
+
+void computerChoice(char computer) {
+
+    srand(time(0));
+
+    int num = rand() % 3 + 1;
+
+    switch(num) {
+        case 1:
+            computer == 'r';
+            std::cout<<"Computer has selected rock";
+            break;
+        case 2:
+            computer == 'p';
+            std::cout<<"Computer has selected paper";
+            break;
+        case 3:
+            computer == 's';
+            std::cout<<"Computer has selected scissors";
+            break;
+    }
 }

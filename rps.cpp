8227d0030384d91a938d3e5fc97ce3f477ad6005
Jenushan Premkumar <jenushan447@gmail.com>
2024-07-27 11:13:25 -0400

    winGame function added

--- rps.cpp
+++ rps.cpp
@@ -2,18 +2,27 @@
 #include <ctime>
 #include <cstdlib>
 
-void computerChoice(char computer);
+char computerChoice();
+char playerChoice();
+void winGame(char player, char computer);
 
 int main() {
 
     char computer;
+    char player;
 
     std::cout<<"Welcome to the Rock Paper Scissors Game";
-    computerChoice(computer);
+    computer = computerChoice();
+    player = playerChoice();
+    winGame(player, computer);
+
+    return 0;
 
 }
 
-void computerChoice(char computer) {
+char computerChoice() {
+
+    char computer; 
 
     srand(time(0));
 
@@ -33,4 +42,50 @@ void computerChoice(char computer) {
             std::cout<<"Computer has selected scissors";
             break;
     }
+
+    return 0;
+}
+
+char playerChoice() {
+
+    char player;
+
+    std::cout<<"Please enter your move: \n";
+    std::cin>>player;
+
+    return 0;
+}
+
+void winGame(char player, char computer) {
+
+    switch (player) {
+        case 'r':
+            if (computer == 'r') {
+                std::cout<<"It is a tie";
+            } else if (computer == 'p') {
+                std::cout<<"You lose";
+            } else if (computer == 's') {
+                std::cout<<"You win";
+            }
+            break;
+        case 'p':
+            if (computer == 'p') {
+                std::cout<<"It is a tie";
+            } else if (computer == 's') {
+                std::cout<<"You lose";
+            } else if (computer == 'r') {
+                std::cout<<"You win";
+            }
+            break;
+        case 's':
+            if (computer == 's') {
+                std::cout<<"It is a tie";
+            } else if (computer == 'r') {
+                std::cout<<"You lose";
+            } else if (computer == 'p') {
+                std::cout<<"You win";
+            }
+            break;
+    }
+
 }
